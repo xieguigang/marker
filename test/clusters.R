3968d019bae074edf61f4f21da2bc6c745ca00e9
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-08 00:39:06 +0800

    read data

--- test/clusters.R
+++ test/clusters.R
@@ -3,11 +3,13 @@ require(clustering);
 let data = read.csv(relative_work("umap.csv"), row.names = 1, check.names = FALSE);
 let clusters = as.data.frame(kmeans(data, centers = 2));
 
+clusters[,"Cluster"] = sprintf("class_%s", clusters$Cluster);
+
 write.csv(clusters, file = relative_work("clusters.csv"));
 
 bitmap(file = relative_work("clusters.png")) {
     plot(as.numeric(clusters$dimension_1), as.numeric(clusters$dimension_2), 
-        class = sprintf("class_%s", clusters$Cluster), 
+        class = clusters[,"Cluster"], 
         colors = "paper",
         fill = "white",
         point.size = 8
---
+++ test/test1.R
@@ -0,0 +1,4 @@
+require(marker);
+
+expr = read.csv("aa55ac5e-7a48-45fd-9fb5-7e013805b247.csv", row.names = 1, check.names = FALSE);
+clusters = read.csv("clusters.csv", row.names = 1, check.names = FALSE);
