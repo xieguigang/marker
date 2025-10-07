c7a956c9554a8a8bfb60008d59c34656fe549397
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-07 13:46:53 +0800

    Update combine_txt.R

--- docs/combine_txt.R
+++ docs/combine_txt.R
@@ -1,5 +1,8 @@
 setwd(relative_work());
 
+# a helper script for combine all R script files as a single text file
+# for run deepseek analysis
+
 let R = list.files("../R", pattern = "*.R");
 let txt = sapply(R, filepath -> readText(filepath));
 
